932aafb8c7226cfd85e1cce19bdecb5a13015799
polinaKl2101 <pklopunova@gmail.com>
2023-06-02 17:03:08 +0600

    add HW3 part 4 and media/

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,8 +1,9 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.filter(needs_autoescape=True)
-def mediapath():
-    pass
+def mediapath(image_path):
+    return f"{settings.MEDIA_URL}{image_path}"
