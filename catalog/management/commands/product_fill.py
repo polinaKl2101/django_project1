718a4c4c31e5af125dad08dad11827f6e8fffd84
polinaKl2101 <pklopunova@gmail.com>
2023-06-04 13:55:07 +0600

    fix images

--- catalog/management/commands/product_fill.py
+++ catalog/management/commands/product_fill.py
@@ -13,10 +13,10 @@ class Command(BaseCommand):
         materials = Category.objects.get(category_name='Материал')
 
         products_list = [
-            {'products_name': 'BMW', 'description': 'white', 'category': autos, 'price_per_purchase': 5000000},
-            {'products_name': 'Milk', 'description': 'coconut', 'category': products, 'price_per_purchase': 50},
-            {'products_name': 'Jeans', 'description': 'levis', 'category': goods, 'price_per_purchase': 3579},
-            {'products_name': 'Gold', 'description': 'gold', 'category': materials, 'price_per_purchase': 6666666},
+            {'products_name': 'BMW', 'product_image': 'prod_images/bmw.jpg', 'description': 'white', 'category': autos, 'price_per_purchase': 5000000},
+            {'products_name': 'Milk', 'product_image': 'prod_images/milk.jpg', 'description': 'coconut', 'category': products, 'price_per_purchase': 50},
+            {'products_name': 'Jeans', 'product_image': 'prod_images/jeans.jpg', 'description': 'levis', 'category': goods, 'price_per_purchase': 3579},
+            {'products_name': 'Gold', 'product_image': 'prod_images/gold.jpg', 'description': 'gold', 'category': materials, 'price_per_purchase': 6666666},
         ]
 
         Product.objects.all().delete()
